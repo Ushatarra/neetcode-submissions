dae1e39cbeaacbf1afb19efb622a4354b1057092
TARRA USHA <ushatarra2306@gmail.com>
2026-07-29 23:47:39 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        s="".join(ch for ch in s if ch.isalnum())
+        s=s.lower()
+        l,r=0,len(s)-1
+
+        if len(s)%2==0:
+            while l < r:
+                if s[l] == s[r]:
+                    l+=1
+                    r-=1
+                else:
+                    return False
+            return True
+        else:
+            while l!=r:
+                if s[l] == s[r]:
+                    l+=1
+                    r-=1
+                else:
+                    return False
+            return True
+
+            
+
+        
